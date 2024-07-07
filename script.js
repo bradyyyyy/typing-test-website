0a6956ef1354debf4b3f9eb2600fb59bf72e8fe5
bradyyyyy <batojt@gmail.com>
2024-07-06 23:56:25 -0500

    Fix return button functionality and resolve early redirect bug
    - Fixed the return button functionality, so it now correctly returns you to index.html
    - Fixed an issue where the user would be redirected to the results screen before they were finished typing the text

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
-const typedText = document.getElementById('typed-text');
-const redirectButton = document.getElementById('redirect-button');
-let typingStarted = false;
-let initialTime, endingTime;
-var count = 0;
+document.addEventListener('DOMContentLoaded', (event) => {
+    const typedText = document.getElementById('typed-text');
+    const redirectButton = document.getElementById('redirect-button');
+    let typingStarted = false;
+    let initialTime, endingTime;
 
-typedText.addEventListener('keydown', (event) => {
-    console.log('Key: ' +event.key);
+    if (typedText) {
+        typedText.addEventListener('input', (event) => {
 
-    if (!typingStarted) {
-        initialTime = new Date();
-        typingStarted = true;
-        console.log('Typing started at: ' + initialTime);
+            if (!typingStarted) {
+                initialTime = new Date();
+                typingStarted = true;
+                console.log('Typing started at: ' + initialTime);
+            }
+
+            let maxLength = document.getElementById('text-to-type').innerText.length;
+            let currentLength = document.getElementById('typed-text').innerText.length;
+
+            console.log('currentLength:' + currentLength);
+            if (currentLength == maxLength) {
+                endingTime = new Date();
+                console.log('Typing finished at: ' + endingTime);
+                showResults();
+            }
+        });
+
+        typedText.addEventListener('keydown', (event) => {
+            console.log('Key: ' + event.key);
+        });
     }
 
-    let maxLength = document.getElementById('text-to-type').innerText.length;
-    let currentLength = document.getElementById('typed-text').innerText.length; 
-
-    console.log('currentLength:' +currentLength);
-    if (currentLength == maxLength-1) {
-        endingTime = new Date();
-        console.log('Typing finished at: ' + endingTime);
-        showResults();
+    function calculateTime() {
+        console.log('Testing');
     }
-        
-});
 
-function showResults () {
-    location.replace('result.html')
-}
+    function showResults() {
+        location.replace('result.html');
+        calculateTime();
+    }
 
-redirectButton.addEventListener('click', () => {
-    location.replace('index.html');
+    if (redirectButton) {
+        redirectButton.addEventListener('click', () => {
+            location.replace('index.html');
+        });
+    }
 });
